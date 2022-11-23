74d7b8be0f3e5de65f72274ab9be59907375143c
BrunaCardoso1998 <112495185+BrunaCardoso1998@users.noreply.github.com>
2022-11-23 08:50:05 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Swiper JS
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+
